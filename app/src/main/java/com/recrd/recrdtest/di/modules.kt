c2013738b2c142c21ad21270c848a8ff4b10421f
mac <opeyemi.adewole@moneco.app>
2023-02-21 12:38:29 +0100

    SetUp koin for DI

--- app/src/main/java/com/recrd/recrdtest/di/modules.kt
+++ app/src/main/java/com/recrd/recrdtest/di/modules.kt
@@ -1,7 +1,13 @@
 package com.recrd.recrdtest.di
 
+import com.recrd.recrdtest.data.http.HttpClient
+import com.recrd.recrdtest.data.http.HttpClientImpl
 import org.koin.dsl.module
 
 val AppModule = module {
 
+}
+
+val DataModule = module {
+    factory<HttpClient> { HttpClientImpl() }
 }
